223f72a11a81c93a40a243d94c00fc9813ae21fb
aksathua21 <aksathua21@gmail.com>
2020-08-03 16:45:24 +0530

    Added Disboard Controller

---
+++ lms/src/main/java/com/harman/lms/controller/Disboard.java
@@ -0,0 +1,8 @@
+package com.harman.lms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class Disboard {}
